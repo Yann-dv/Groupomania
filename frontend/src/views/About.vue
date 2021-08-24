f81cf213d2cf1075540f8d044f025a071de61d48
Yann Dev <h.yann_pro@yahoo.fr>
2021-08-25 00:58:47 +0200

    App creation, using vue CLI for frontend and npm init for back

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Groupomania - À propos</h1>
+  </div>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/header_logo.png">  Passé avant les onglets de vues-->
+    <HelloWorld msg="Bienvenue sur le réseau social interne de Groupomania !"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
