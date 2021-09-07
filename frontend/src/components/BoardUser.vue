7c969c3f73809c5835685df451175e3c064f98e5
Yann Dev <h.yann_pro@yahoo.fr>
2021-09-07 12:18:15 +0200

    Implement modal form and style for modifying

--- frontend/src/components/BoardUser.vue
+++ frontend/src/components/BoardUser.vue
@@ -25,6 +25,7 @@
         <li v-for="item in apiAllArticles" :key="item" class="py-3">
           <div class="card rounded">
             <div class="card-body bg-light">
+            <!--Card header-->
               <div class="card-header rounded mb-3 position-relative">
                 <span class="card-title mainColored text-light rounded-pill p-2 fw-bold">
                 <font-awesome-icon icon="user"/>
@@ -32,36 +33,53 @@
                 </span>
                 <span class="ps-2 card-subtitle text-underline fw-bold secondColored">{{ item.title }}</span>
                 <span class="px-5 float-right position-absolute end-0 disabled text-muted">
-                Publié il y a {{ postedSince }} jours</span>
+                    Publié il y a {{ postedSince }} jours</span>
                 <div class="btn-group position-absolute end-0 top-0" role="group">
                   <button v-if="currentUser.username === item.authorName && currentUser.id === item.authorId" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" id ="btnGroupDropClose" aria-label="Close">
                   </button>
                   <ul class="dropdown-menu" aria-labelledby="btnGroupDropClose">
                   <li v-on:click="modalModifyPost = true" class="dropdown-item">Modifier mon post</li>
-                  <!--Modal content for modify user's articles-->
+                  <li v-on:click="deletePost" class="dropdown-item">Supprimer mon post</li>
+                </ul>
+                </div>
+              </div><!--header end-->
+              <!--Card article content-->
+              <div class="articleContent">
+                <p class="card-text px-5 py-2">{{ item.content }}</p>
+                <font-awesome-icon icon="home"/>
+              </div>
+              
+            </div><!--Body end-->
+           </div><!--Card end-->
+          <!--Modal content for modify user's articles-->
                   <teleport to="#modifyArticles">
                     <div v-if="modalModifyPost" class="modal">
-                      <div class="card rounded">
+                      <div class="card rounded mx-auto col-12 col-md-8">
                         <div class="card-body bg-light">
                           <div class="card-header rounded mb-3 position-relative">
-                            <h3>Modifier ma publication</h3>
-                              <button type="button" v-on:click="modalModifyPost = false" class="btn-close" aria-label="Annuler"></button>
+                            <span class="card-title rounded-pill p-2 fw-bold"><font-awesome-icon icon="user"/>
+                            {{ currentUser.username }}</span>
+                            <h5>Modifier ma publication : </h5>
+                            <div class="form form-floating mx-auto col-12 col-md-8">
+                                <div class="modifyMyArticle">
+                                  <textarea  class="form-control rounded"
+                                  style="min-height:14rem" 
+                                  v-model="item.content"
+                                   id="modifyMyArticleTextArea">
+                                  </textarea>
+                                </div>
+                            </div>
+                          <div class="mt-3 card-buttons d-flex justify-content-between">
+                            <button type="button" v-on:click="modalModifyPost = false" class="btn-close" aria-label="Annuler"></button>   
+                            <button class="btn btn-primary rounded-pill" type="submit">Valider</button>
+                          </div>
                           </div>
                         </div>
                       </div>
                     </div>
                     </teleport>
-                  <li v-on:click="deletePost" class="dropdown-item">Supprimer mon post</li>
-                </ul>
-                </div>
-              </div><!--header end-->
-              <div class="articleContent">
-                <p class="card-text px-5 py-2">{{item.content}}</p>
-                <font-awesome-icon icon="home"/>
-              </div>
-            </div>
-           </div>
-        </li>
+                <!--Modal end-->
+          </li>
         </ul>
         </div> <!--Grid row end-->
       </div>
@@ -82,8 +100,8 @@ export default {
   },
   data() {
     return {
+      modifyMyArticleFrom: "",
       modalModifyPost: false,
-      modal2: false,
       loading: false,
       headerColor: "#8957E5",
       mainColor: "#122442",
@@ -182,8 +200,8 @@ li {
   align-items: center;
   justify-content: center;
   background-color: white;
-  width: 300px;
-  height: 300px;
+  width: 20vw;
+  height: 20vh;;
   padding: 5px;
 }
 </style>
