373ca4323d15443674e3b28693b4cae80db94493
Yann Dev <h.yann_pro@yahoo.fr>
2021-09-18 02:46:27 +0200

    Create messages done, switch the form to article.vue

--- frontend/src/components/Messages.vue
+++ frontend/src/components/Messages.vue
@@ -6,7 +6,9 @@
         v-for="message in apiAllMessages.rows"
         :key="message"
         v-bind:id="message.id"
+        :prop="message.linkedArticle"
         class="collapsed-item my-2"
+
       >
         <!--<div v-if="ArticleService.getOneArticle(message.linkedArticle) === message.linked"-->
         <div class="collapsed-card card card-body">
@@ -29,68 +31,24 @@
         </div>
       </li>
       <!--Message list end-->
-      <Form
-        @submit="messageSubmit()"
-        :validation-schema="messageSchema"
-        id="messageForm"
-      >
-        <div class="form-group form-floating">
-          <Field
-            type="text"
-            class="form-control newMessage"
-            v-model="content"
-            name="newMessage"
-          />
-          <label for="newMessage" class="text-decoration-underline"
-            >Répondre:</label
-          >
-          <ErrorMessage
-            name="newMessage"
-            class="error-feedback ms-3"
-            style="color:red"
-          />
-        </div>
-        <div class="send-btn form-group">
-          <button class="btn btn-primary rounded my-1" type="submit"
-          >
-            <span
-              v-show="loading"
-              class="spinner-border spinner-border-sm"
-            ></span>
-            Poster
-          </button>
-        </div>
-      </Form>
+      
+
     </ul>
   </div>
 </template>
 
 <script>
 import MessageService from "../services/messages-service";
-import { Form, Field, ErrorMessage } from "vee-validate";
-import * as yup from "yup";
 
 export default {
   name: "Messages",
-  components: {
-    Form,
-    Field,
-    ErrorMessage,
-  },
   props: ['apiAllMessages'],
   data() {
-    const messageSchema = yup.object().shape({
-      newMessage: yup
-        .string()
-        .min(1, "Veuillez écrire au moins un caractère")
-        .max(60, "Veuillez écrire une réponse plus courte (max 60 caractères"),
-    });
     return {
-      messageSchema,
       loading: false,
       msgFromApi: "",
       content:"",
-      linkedArticle: "",
+      linkedArticle:"",
     };
   },
   mounted() {
@@ -110,20 +68,6 @@ export default {
         "-" +
         current_datetime.getFullYear();
       return formatted_date;
-    },
-    messageSubmit() {
-      if (this.content) {
-        MessageService.createMessage({
-          linkedArticle: event.target.parentElement.linkedArticle,
-          content: this.content,
-        })
-        .then(() => {
-          setTimeout(function(){
-            window.location.reload(1);
-          }, 300);
-        })
-      }
-
     },
     messageDelete(messageToDelete) {
       if (confirm("Souhaitez-vous vraiment supprimer ce message ?")) {
